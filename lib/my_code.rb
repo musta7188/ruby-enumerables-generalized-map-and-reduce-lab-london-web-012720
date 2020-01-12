b04c9f8370644926087961969e027482e9c92b97
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-12 10:39:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+
+def map(array)
+
+  new_array = []
+
+  index = 0
+
+  while index < array.length do
+  new_array <<  yield(array[index])
+    index += 1
+  end
+new_array
+end
+
+def reduce(array, starting_point = nil)
+
+total = []
+index = 0
+
+while index < array.length do
+
+total << yield(starting_point + array[index])
+
+index += 1
+
+end
+total
+end
